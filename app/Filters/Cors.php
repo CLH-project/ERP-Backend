aa4b2a1a9cdc8ce874b39df67a5d48c9c235204f
Carlos Henrique <carlinhosbackend@gmail.com>
2025-08-15 19:32:41 -0300

    feat: cors2

---
+++ app/Filters/Cors.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Filters;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class Cors implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        header('Access-Control-Allow-Origin: http://localhost:3000');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+        if ($request->getMethod() === 'options') {
+            header('HTTP/1.1 200 OK');
+            exit;
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        $response->setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
