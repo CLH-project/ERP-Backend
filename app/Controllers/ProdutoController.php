1e2469c934014cc7adfd88053a1ba75818336e45
Carlos Henrique <carlinhosbackend@gmail.com>
2025-08-27 21:26:12 -0300

    feat: criando model de fornecedor

---
+++ app/Controllers/ProdutoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\ProdutoModel;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class ProdutoController extends BaseController
+{
+    public function create()
+    {
+        
+    }
+}
