24641f041d8b1aa6521009cdabbc92e2460a3db5
Carlos Henrique <carlinhosbackend@gmail.com>
2025-10-04 11:48:50 -0300

    feat: Cors e filters modificados

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -1,27 +1,34 @@
 <?php
+
 namespace App\Filters;
+
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
 
-class Cors implements FilterInterface
+class CorsFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
     {
-        header('Access-Control-Allow-Origin: http://localhost:3000');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+        $response = service('response');
+
+        $response->setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+        $response->setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->setHeader('Access-Control-Allow-Credentials', 'true');
 
         if ($request->getMethod() === 'options') {
-            header('HTTP/1.1 200 OK');
-            exit;
+            return $response->setStatusCode(200);
         }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
         $response->setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-        $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
+        $response->setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->setHeader('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
     }
 }
